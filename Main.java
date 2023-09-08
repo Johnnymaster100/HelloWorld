b7850c8e0343e950f06f1afbefc8c2cfada04254
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2023-09-08 17:05:04 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello, World!");
+    System.out.println("My name is Jonathan and I am a junior at Brooklyn Technical High School.");
+    System.out.println("I have a few hobbies. For example, I am a magician and I love to entertain people with lots of tricks!");
+    System.out.println("I am also a competitive swimmer. Overall, I have swam for over 10 years!");
+    System.out.print("Well, that's all for now. And remember, don't have a good day, have a GREAT day!");
+  }
+}
