d98e7906c6977354fb51b767e462b3480aea3ae1
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2023-09-11 15:15:00 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
-public class Main
-{
-  public static void main(String[]args)
-  {
+public class Main {
+  
+  public static void main(String[]args) {
+    
     System.out.println("Hello, World!");
     System.out.println("My name is Jonathan and I am a junior at Brooklyn Technical High School.");
     System.out.println("I have a few hobbies. For example, I am a magician and I love to entertain people with lots of tricks!");
     System.out.println("I am also a competitive swimmer. Overall, I have swam for over 10 years!");
-    System.out.print("Well, that's all for now. And remember, don't have a good day, have a GREAT day!");
+    System.out.println("Well, that's all for now. And remember, don't have a good day, have a GREAT day!\n");
+    
+    int x = 12;
+    double y = 0.123456789;
+    final boolean z = false;
+    
+    System.out.println(x);
+    System.out.println(y);
+    System.out.print(z);
   }
 }
