fbcfa566d86c090565ccc3817aabf5670f1c20cd
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2023-09-20 18:45:22 -0400

    Added a random number system to generate a random card

--- Main.java
+++ Main.java
@@ -1,26 +1,41 @@
+import java.util.Random;
+
 public class Main {
   
-  public static String division(int i, int j){
-    if (j == 0) {
-      return "Cannot divide by zero!"; }
-    else {
-      return Integer.toString(i / j); }
-  }
-  
   public static void main(String[]args) {
-    
-    Card card = new Card("Five", "Diamonds");
-    
-    Card worth = new Card(5);
-    
-    System.out.println("Your card is the " + card.getCard() + "!");
-    
-    if (worth.getWorth() == 1)
-    {
-      System.out.println("Your card is worth " + worth.getWorth() + " point.");
+    Random rand = new Random();
+    int rand_int = rand.nextInt(13) + 1;
+    String card_value = Integer.toString(rand_int);
+    if (card_value.equals("1")) {
+      card_value = "Ace";
     }
-    else {
-      System.out.println("Your card is worth " + worth.getWorth() + " points.");
+    else if (card_value.equals("11")) {
+      card_value = "Jack";
     }
+    else if (card_value.equals("12")) {
+      card_value = "Queen";
+    }
+    else if (card_value.equals("13")) {
+      card_value = "King";
+    }
+    
+    rand_int = rand.nextInt(4) + 1;
+    String card_suit = Integer.toString(rand_int);
+    if (card_suit.equals("1")) {
+      card_suit = "Spades";
+    }
+    else if (card_suit.equals("2")) {
+      card_suit = "Hearts";
+    }
+    else if (card_suit.equals("3")){
+      card_suit = "Diamonds";
+    }
+    else if (card_suit.equals("4")) {
+      card_suit = "Clubs";
+    }
+    
+    Card rand_card = new Card(card_value, card_suit);
+    
+    System.out.println("Your randomly generated card is the: " + rand_card.getCard());
   }
 }
