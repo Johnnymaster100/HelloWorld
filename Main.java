b3d89f217a66cfda2de4d560c76beb641f1d9f53
Jonathan Shamis <127160756+Johnnymaster100@users.noreply.github.com>
2023-09-29 15:13:12 -0400

    Added the full functionality of a random card generator
    1) Asks the user to input how many random cards they want to generate.
    
    2) Generates that amount of random cards, with no repeats.
    
    3) Repeats this process until the user wants to quit the program.

--- Main.java
+++ Main.java
@@ -1,41 +1,102 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-  
-  public static void main(String[]args) {
-    Random rand = new Random();
-    int rand_int = rand.nextInt(13) + 1;
-    String card_value = Integer.toString(rand_int);
-    if (card_value.equals("1")) {
-      card_value = "Ace";
+
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        Deck deck = new Deck();
+        boolean repeat = true;
+        ArrayList<String> random_cards = new ArrayList<String>();
+
+        System.out.println("Welcome to the Random Card Generator!");
+        System.out.println("This random generator allows you to generate up to 13 random cards from a deck of cards, at once!");
+
+        while(repeat) {
+            random_cards.clear();
+            System.out.println("Please enter how many random cards you would like to generate: ");
+
+            int num_cards = scanner.nextInt();
+
+            for (int i = 0; i < num_cards; i++) {
+                int rand_int = rand.nextInt(52 - i);
+                String abbrev_card = deck.randomCard(rand_int);
+                String card_value;
+                String card_suit = null;
+
+                if (abbrev_card.charAt(0) == 'A') {
+                    card_value = "Ace";
+                } else if (abbrev_card.charAt(0) == 'J') {
+                    card_value = "Jack";
+                } else if (abbrev_card.charAt(0) == 'Q') {
+                    card_value = "Queen";
+                } else if (abbrev_card.charAt(0) == 'K') {
+                    card_value = "King";
+                } else if (abbrev_card.startsWith("10")) {
+                    card_value = "10";
+                } else {
+                    card_value = abbrev_card.substring(0, 1);
+                }
+
+                if (abbrev_card.charAt(1) == 'S') {
+                    card_suit = "Spades";
+                } else if (abbrev_card.charAt(1) == 'H') {
+                    card_suit = "Hearts";
+                } else if (abbrev_card.charAt(1) == 'C') {
+                    card_suit = "Clubs";
+                } else if (abbrev_card.charAt(1) == 'D') {
+                    card_suit = "Diamonds";
+                } else {
+                    if (abbrev_card.charAt(2) == 'S') {
+                        card_suit = "Spades";
+                    } else if (abbrev_card.charAt(2) == 'H') {
+                        card_suit = "Hearts";
+                    } else if (abbrev_card.charAt(2) == 'C') {
+                        card_suit = "Clubs";
+                    } else if (abbrev_card.charAt(2) == 'D') {
+                        card_suit = "Diamonds";
+                    }
+                }
+
+                Card random_card = new Card(card_value, card_suit);
+                random_cards.add(random_card.getCard());
+                deck.removeCard(rand_int);
+            }
+
+            System.out.println();
+            System.out.println("Your randomly generated cards are:");
+            System.out.println();
+            for (int i = 0; i < num_cards; i++) {
+                System.out.println((i + 1) + ": " + random_cards.get(i));
+            }
+
+            deck.resetDeck();
+
+            Scanner new_scanner = new Scanner(System.in);
+            System.out.println();
+            System.out.println("Would you like to generate a new set of numbers: y/n");
+            String prompt_repeat = new_scanner.nextLine();
+
+            if (prompt_repeat.equals("y")) {
+                repeat = true;
+            }
+            else if (prompt_repeat.equals("n")){
+                repeat = false;
+            }
+            System.out.println();
+
+        }
+        System.out.println("Thank you for using the 'Random Card Generator'!");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("And remember, don't have a good day, have a GREAT day!");
+
     }
-    else if (card_value.equals("11")) {
-      card_value = "Jack";
-    }
-    else if (card_value.equals("12")) {
-      card_value = "Queen";
-    }
-    else if (card_value.equals("13")) {
-      card_value = "King";
-    }
-    
-    rand_int = rand.nextInt(4) + 1;
-    String card_suit = Integer.toString(rand_int);
-    if (card_suit.equals("1")) {
-      card_suit = "Spades";
-    }
-    else if (card_suit.equals("2")) {
-      card_suit = "Hearts";
-    }
-    else if (card_suit.equals("3")){
-      card_suit = "Diamonds";
-    }
-    else if (card_suit.equals("4")) {
-      card_suit = "Clubs";
-    }
-    
-    Card rand_card = new Card(card_value, card_suit);
-    
-    System.out.println("Your randomly generated card is the: " + rand_card.getCard());
-  }
 }
